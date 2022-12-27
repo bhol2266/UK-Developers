dda0405b918d337b11df5fd6302651f6524c92d7
baldgenie <siddhartha.kapoor@gmail.com>
2022-12-27 23:13:00 +0545

    commit

--- pages/index.js
+++ pages/index.js
@@ -6,19 +6,14 @@ export default function Home() {
   return (
     <div>
 
-      <div className='relative'>
-
-        <img src="./homepage_bg.png" alt="" className='absolute  top-0 left-0 right-0 -z-10' />
+      <div className='relative bg-banner w-full bg-no-repeat bg-cover h-[300px] lg:h-[400px] xl:h-[600px] space-x-4 flex items-center justify-center'>
 
 
-        <div className='absolute -bottom-[250px] right-0 left-0 space-x-4 flex items-center justify-center'>
+        <button className='hover:bg-gray-600 font-inter font-medium  px-4 py-2 rounded-[5px] text-white bg-textMain'>Discover more</button>
 
-          <button className='hover:bg-gray-600 font-inter font-medium  px-4 py-2 rounded-[5px] text-white bg-textMain'>Discover more</button>
-
-          <button className='hover:bg-gray-600 font-inter font-medium  px-4 py-2 rounded-[5px] text-white bg-textMain'>Services</button>
-        </div>
-
+        <button className='hover:bg-gray-600 font-inter font-medium  px-4 py-2 rounded-[5px] text-white bg-textMain'>Services</button>
       </div>
+
     </div>
   )
 }
