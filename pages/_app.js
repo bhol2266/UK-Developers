9aed9c2354313ea1de55c10ccc4b17c2a222b15d
baldgenie <siddhartha.kapoor@gmail.com>
2023-01-01 13:19:14 +0545

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -43,15 +43,15 @@ export default function App({ Component, pageProps }) {
       <Navbar />
       <Component {...pageProps} />
 
-      <div className={`${messageBox ? "bg-white" : ""} shadow rounded-lg py-4 px-1 cursor-pointer  fixed bottom-2 left-3 z-10 lg:bottom-5 lg:left-5 right-3 lg:right-0 flex items-center space-x-3 lg:space-x-4`}>
+      <div className={`${messageBox ? "bg-white" : ""} shadow rounded-lg py-4  cursor-pointer  fixed bottom-2 left-2 z-10 lg:bottom-5 lg:left-5 right-3 lg:right-0 flex items-center space-x-3 lg:space-x-4`}>
         <ReactWhatsapp id='ReactWhatsapp'  number="91-910-882-5914" message={message} />
         <img onClick={openMessageBox} src="./whatsapp.svg" alt="" className='h-[50px]  lg:h-[80px] ' />
 
 
         {messageBox &&
-          <div className=''>
+          <div className='w-fit'>
             <h1 className='font-inter text-[14px] lg:text-[16px] font-medium mb-1'>Chats with us! (online)</h1>
-            <input onChange={(e) => { setmessage(e.target.value) }} value={message} type="text" name="message" id="message" placeholder='Enter message' className='px-2 py-1 lg:px-3 lg:py-2 border-[1px] border-[#25D366] rounded-md text-[#075E54] font-inter text-[14px] outline-none' />
+            <input onChange={(e) => { setmessage(e.target.value) }} value={message} type="text" name="message" id="message" placeholder='Enter message' className='px-2 py-1 lg:px-3 lg:py-2 border-[1px] border-[#25D366] rounded-md text-[#075E54] font-inter text-[14px] outline-none ' />
 
             <button onClick={whatsAppClick} className='px-4 py-2 text-white font-inter font-medium bg-[#25D366] rounded-md mx-2 lg:mx-4 text-[12px] lg:text-[14px]'>Start</button>
 
