706190278fb7c6eebbece3df590a603813528ea9
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2024-08-20 00:19:18 +0545

    sdafdasf

--- pages/chutlunds.js
+++ pages/chutlunds.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { scrapeVideos } from '../spangbang';
 
-export default function Chutunds({ video_collection, pages, desiVideosDataArray, desiMmsVideoArray }) {
-    return (
-        <div>{JSON.stringify(video_collection)}</div>
-    )
+export default function Chutunds({ finalDataArray }) {
+  return (
+    <div>{JSON.stringify(finalDataArray)}</div>
+  )
 }
 
 
@@ -13,37 +13,23 @@ export async function getServerSideProps({ req, res }) {
 
 
 
-    const parcelData = { href: "https://spankbang.party/" }
-    const rawResponse = await fetch(`https://sheppards-rust.vercel.app/api/spangbang/homepage`, {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      method: 'POST',
-      body: JSON.stringify(parcelData),
-    });
-    const ress = await rawResponse.json();;
-    const finalDataArray_Arrar=ress.finalDataArray;
-  
-  
-    var desiVideosDataArray = []
-    const obj = await scrapeVideos(`https://spankbang.party/s/desi%20sex%20videos/?o=all`)
-    desiVideosDataArray = obj.finalDataArray
-  
-    var desiMmsVideoArray = []
-    const obj2 = await scrapeVideos(`https://spankbang.party/s/desi%20mms/?o=all`)
-    desiMmsVideoArray = obj2.finalDataArray
-  
-  
-  
-    return {
-      props: {
-        video_collection: finalDataArray_Arrar,
-        desiVideosDataArray: desiVideosDataArray,
-        desiMmsVideoArray: desiMmsVideoArray
-      }
+  var finalDataArray = []
+  var pages = []
+
+
+
+  console.log(`https://spankbang.party/`)
+  const obj = await scrapeVideos(`https://spankbang.party/`)
+  finalDataArray = obj.finalDataArray
+  pages = obj.pages
+
+
+  return {
+    props: {
+      finalDataArray: finalDataArray,
+      pages: pages
     }
-  
-  
-  
   }
+
+
+}
