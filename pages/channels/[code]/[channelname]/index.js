24777297e31bc00fc7444af4fa51766b7ff3a853
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2024-08-20 19:56:15 +0545

    dfsg

--- pages/channels/[code]/[channelname]/index.js
+++ pages/channels/[code]/[channelname]/index.js
@@ -20,7 +20,7 @@ function Index({ body}) {
     }
 
 
-    
+
 
     return (
         <>
@@ -41,25 +41,25 @@ function Index({ body}) {
 export default Index
 
 
-// export async function getStaticPaths() {
-//     return {
-//         paths: [
-//             {
-//                 params: {
-//                     code: 'l3',
-//                     channelname: 'kink+com'
-//                 }
-//             }
-//         ],
-//         fallback: true // false or 'blocking'
-//     };
-// }
+export async function getStaticPaths() {
+    return {
+        paths: [
+            {
+                params: {
+                    code: 'l3',
+                    channelname: 'kink+com'
+                }
+            }
+        ],
+        fallback: true // false or 'blocking'
+    };
+}
 
 
 
 
 
-export async function getServerSideProps(context) {
+export async function getStaticProps(context) {
 
     const { code, channelname } = context.params;
 
